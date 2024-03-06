7a1ece6f860eda4fa570486085abb4f7ab45a9e3
Teja Mr <teja.maridu@gmail.com>
2024-03-06 10:16:42 -0700

    Removing project old files

--- src/app/shared/login.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-    providedIn: 'root'
-})
-export class LoginService {
-    startupVisited: boolean = false;
-    isLoggedin: boolean = false;
-}
